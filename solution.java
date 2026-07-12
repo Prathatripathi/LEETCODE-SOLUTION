2f1a5fa4f4c10d896716aa660497a3efac6973d4
Prathatripathi <prathat49@gmail.com>
2026-07-12 16:09:57 +0530

    Solution 200

--- solution.java
+++ solution.java
@@ -1,90 +1,25 @@
 class Solution {
-
-    List<List<String>> ans = new ArrayList<>();
-    List<String> path = new ArrayList<>();
-    Map<String, List<String>> parent = new HashMap<>();
-
-    public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
-
-        Set<String> dict = new HashSet<>(wordList);
-
-        if (!dict.contains(endWord))
-            return ans;
-
-       
-        Queue<String> q = new LinkedList<>();
-        Map<String, Integer> level = new HashMap<>();
-
-        q.offer(beginWord);
-        level.put(beginWord, 0);
-
-        while (!q.isEmpty()) {
-
-            String word = q.poll();
-            int currLevel = level.get(word);
-
-            char[] arr = word.toCharArray();
-
-            for (int i = 0; i < arr.length; i++) {
-
-                char old = arr[i];
-
-                for (char ch = 'a'; ch <= 'z'; ch++) {
-
-                    arr[i] = ch;
-                    String next = new String(arr);
-
-                    if (!dict.contains(next))
-                        continue;
-
-                    
-                    if (!level.containsKey(next)) {
-
-                        level.put(next, currLevel + 1);
-                        q.offer(next);
-
-                        parent.putIfAbsent(next, new ArrayList<>());
-                        parent.get(next).add(word);
-                    }
-
-                   
-                    else if (level.get(next) == currLevel + 1) {
-
-                        parent.get(next).add(word);
-                    }
+    public int numIslands(char[][] grid) {
+        int cnt=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]=='1'){
+                    cnt++;
+                    dfs(grid,i,j);
                 }
-
-                arr[i] = old;
             }
         }
-
-        if (!level.containsKey(endWord))
-            return ans;
-
-        path.add(endWord);
-        dfs(endWord, beginWord);
-
-        return ans;
+        return cnt;
     }
-
-    void dfs(String word, String beginWord) {
-
-        if (word.equals(beginWord)) {
-
-            List<String> temp = new ArrayList<>(path);
-            Collections.reverse(temp);
-            ans.add(temp);
-            return;
-        }
-
-        if (!parent.containsKey(word))x
-            return;
-
-        for (String par : parent.get(word)) {
-
-            path.add(par);
-            dfs(par, beginWord);
-            path.remove(path.size() - 1);
-        }
+    public void dfs (char[][]grid,int i,int j){
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length)
+         return ;
+        if(grid[i][j]=='0')
+         return;
+        grid[i][j]='0';
+        dfs(grid,i+1,j);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j-1);
+        dfs(grid,i,j+1);
     }
 }

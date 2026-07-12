5b08810dc157e9af2920e1c5b84149c961179b4b
Prathatripathi <prathat49@gmail.com>
2026-07-12 15:49:39 +0530

    Solution 126

--- solution.java
+++ solution.java
@@ -1,47 +1,90 @@
 class Solution {
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Queue<Pair<String, Integer>> q = new LinkedList<>();
-        q.add(new Pair<>(beginWord, 1));
 
-        Set<String> st = new HashSet<>(wordList);
-        st.remove(beginWord);
+    List<List<String>> ans = new ArrayList<>();
+    List<String> path = new ArrayList<>();
+    Map<String, List<String>> parent = new HashMap<>();
+
+    public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+
+        Set<String> dict = new HashSet<>(wordList);
+
+        if (!dict.contains(endWord))
+            return ans;
+
+       
+        Queue<String> q = new LinkedList<>();
+        Map<String, Integer> level = new HashMap<>();
+
+        q.offer(beginWord);
+        level.put(beginWord, 0);
 
         while (!q.isEmpty()) {
-            String word = q.peek().getKey();
-            int steps = q.peek().getValue();
-            q.poll();
 
-         
-            if (word.equals(endWord)) return steps;
+            String word = q.poll();
+            int currLevel = level.get(word);
 
-      
-            for (int i = 0; i < word.length(); i++) {
-                char[] arr = word.toCharArray();
-                char original = arr[i];
+            char[] arr = word.toCharArray();
+
+            for (int i = 0; i < arr.length; i++) {
+
+                char old = arr[i];
+
                 for (char ch = 'a'; ch <= 'z'; ch++) {
+
                     arr[i] = ch;
-                    String newWord = new String(arr);
-                    if (st.contains(newWord)) {
-                        st.remove(newWord);
-                        q.add(new Pair<>(newWord, steps + 1));
+                    String next = new String(arr);
+
+                    if (!dict.contains(next))
+                        continue;
+
+                    
+                    if (!level.containsKey(next)) {
+
+                        level.put(next, currLevel + 1);
+                        q.offer(next);
+
+                        parent.putIfAbsent(next, new ArrayList<>());
+                        parent.get(next).add(word);
+                    }
+
+                   
+                    else if (level.get(next) == currLevel + 1) {
+
+                        parent.get(next).add(word);
                     }
                 }
-                arr[i] = original;
+
+                arr[i] = old;
             }
         }
-        return 0;
+
+        if (!level.containsKey(endWord))
+            return ans;
+
+        path.add(endWord);
+        dfs(endWord, beginWord);
+
+        return ans;
     }
 
+    void dfs(String word, String beginWord) {
 
-class Pair<K, V> {
-    private K key;
-    private V value;
-    public Pair(K key, V value) {
-        this.key = key;
-        this.value = value;
+        if (word.equals(beginWord)) {
+
+            List<String> temp = new ArrayList<>(path);
+            Collections.reverse(temp);
+            ans.add(temp);
+            return;
+        }
+
+        if (!parent.containsKey(word))x
+            return;
+
+        for (String par : parent.get(word)) {
+
+            path.add(par);
+            dfs(par, beginWord);
+            path.remove(path.size() - 1);
+        }
     }
-    public K getKey() { return key; }
-    public V getValue() { return value; }
 }
-        
-    }

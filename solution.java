26b6ddf5522ebe6bc9a28c6cced6f681d51598e9
Prathatripathi <prathat49@gmail.com>
2026-07-12 15:32:56 +0530

    Problem 127

--- solution.java
+++ solution.java
@@ -1,51 +1,47 @@
 class Solution {
-    public void solve(char[][] board) {
-        int m=board.length;
-        int n=board[0].length;
-        int[][] vis=new int[m][n];
-        Queue<int[]>q=new LinkedList<>();
-      for(int j = 0; j < n; j++){
-    if(board[0][j] == 'O'){
-        q.add(new int[]{0, j});
-        vis[0][j] = 1;
-    }
-}
-for(int j = 0; j < n; j++){
-    if(board[m-1][j] == 'O'){
-        q.add(new int[]{m-1, j});
-        vis[m-1][j] = 1;
-    }
-}
-for(int i = 0; i < m; i++){
-    if(board[i][0] == 'O'){
-        q.add(new int[]{i, 0});
-        vis[i][0] = 1;
-    }
-}
-for(int i = 0; i < m; i++){
-    if(board[i][n-1] == 'O'){
-        q.add(new int[]{i, n-1});
-        vis[i][n-1] = 1;
-    }
-}
-        int[][] dis={{0,1},{0,-1},{1,0},{-1,0}};
-        while(!q.isEmpty()){
-            int[] curr=q.poll();
-            int r=curr[0];
-            int c=curr[1];
-            for(int[] d:dis){
-                int row=r+d[0];
-                int col=c+d[1];
-                if(row>=0 && col>=0 && row<m && col<n && vis[row][col]==0 && board[row][col]=='O'){
-                    vis[row][col]=1;
-                    q.add(new int[]{row,col});
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Queue<Pair<String, Integer>> q = new LinkedList<>();
+        q.add(new Pair<>(beginWord, 1));
+
+        Set<String> st = new HashSet<>(wordList);
+        st.remove(beginWord);
+
+        while (!q.isEmpty()) {
+            String word = q.peek().getKey();
+            int steps = q.peek().getValue();
+            q.poll();
+
+         
+            if (word.equals(endWord)) return steps;
+
+      
+            for (int i = 0; i < word.length(); i++) {
+                char[] arr = word.toCharArray();
+                char original = arr[i];
+                for (char ch = 'a'; ch <= 'z'; ch++) {
+                    arr[i] = ch;
+                    String newWord = new String(arr);
+                    if (st.contains(newWord)) {
+                        st.remove(newWord);
+                        q.add(new Pair<>(newWord, steps + 1));
+                    }
                 }
+                arr[i] = original;
             }
         }
-        for(int i=0;i<m;i++)
-         for(int j=0;j<n;j++)
-          if(board[i][j]=='O' && vis[i][j]==0)
-           board[i][j]='X';
+        return 0;
+    }
+
+
+class Pair<K, V> {
+    private K key;
+    private V value;
+    public Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+    public K getKey() { return key; }
+    public V getValue() { return value; }
+}
         
     }
-}
